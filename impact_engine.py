c3871f122ff2abe33aacc108f8ac81e66b25f911
qkdwoals2452-jpg <qkdwoals2452@gmail.com>
2026-07-27 16:51:08 +0900

    Create impact_engine.py

---
+++ impact_engine.py
@@ -0,0 +1,50 @@
+def get_news_impact_score(stock, theme, articles, company):
+    stock_name = stock.get("name", "")
+
+    article_text = ""
+
+    for article in articles:
+        article_text += " " + article.get("title", "")
+        article_text += " " + article.get("summary", "")
+
+    article_text = article_text.lower()
+
+    matched = company.get("matched", {})
+
+    company_words = []
+
+    for words in matched.values():
+        company_words.extend(words)
+
+    company_words = list(set(company_words))
+
+    direct_matched = []
+
+    for word in company_words:
+        if not word:
+            continue
+
+        if word.lower() in article_text:
+            direct_matched.append(word)
+
+    score = 0
+
+    if stock_name and stock_name.lower() in article_text:
+        score += 60
+
+    score += min(len(direct_matched) * 10, 40)
+
+    if score >= 60:
+        memo = "뉴스 직접 수혜 가능성 높음"
+    elif score >= 30:
+        memo = "뉴스 간접 수혜 가능성"
+    else:
+        memo = "뉴스와 직접 연결 약함"
+
+    return {
+        "score": score,
+        "memo": memo,
+        "matched": direct_matched[:10],
+        "exclude": score < 20,
+        "exclude_reason": "기사와 기업의 직접 연결 근거 부족"
+    }
